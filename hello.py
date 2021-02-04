c1db35ec20aa60a6da80626a4b562d346067c1ab
Yuto-Shinohara <yuto.shino0128@gmail.com>
2021-02-04 19:59:44 +0900

    認識結果を送信可能にした。

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
-
-from flask import Flask, render_template #追加
+from flask import Flask, render_template,redirect,request,Blueprint,jsonify,send_from_directory #追加
 import MeCab
 import sys
+import json
+import os
+
 
 
 
@@ -12,8 +14,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = "Hoge"
-    return name
+    name = "音声認識結果"
+    #return name
     #recieve = sys.stdin.readline()
     #recieve = recieve + "OK!"
 
@@ -25,6 +27,21 @@ def hello():
     #print(sentence_wakati)
     return render_template('hello.html', title='flask test', name=name) #変更
 
+@app.route('/ajax_post', methods=['GET','POST'])
+def index():
+    if request.method == "POST":
+        get_data = request.form["voice_input"]
+        print("音声認識結果:" + get_data)
+        if get_data:
+            return jsonify({'output':get_data})
+        return jsonify({'error' : 'Missing data!'})
+
+    #global selected
+    #post = request.args.get('post', 0, type=int)
+    #return json.dumps({'selected post': str(post)});
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path,'static/img'),'favicon.jpeg')
 
 
 
--- static/hello.js
+++ static/hello.js
@@ -2,22 +2,35 @@
 const speech = new webkitSpeechRecognition();
 speech.lang = 'ja-JP';
 
+console.log('hello.jsが読み込まれた');
+
 const btn = document.getElementById('btn');
 const content = document.getElementById('content');
 
 btn.addEventListener('click' , function() {
     speech.start();
 });
-console.log('アイウエオ')
+
+//console.log('アイウエオ')
 speech.onresult = function(e) {
      speech.stop();
      if(e.results[0].isFinal){
          var autotext =  e.results[0][0].transcript
          content.innerHTML += '<div>'+ autotext +'</div>';
          console.log(autotext);
-
-
-
+         //認識した言葉をpythonに渡す
+         $.ajax({
+           url:'/ajax_post',
+           type:'POST',
+           data:{
+             'voice_input':autotext
+           }
+         }).done(function(data){
+           console.log("ajax success:",data);
+           content.innerHTML += '<div>認識結果:'+data.output +'</div>';
+         }).fail(function(){
+           console.log('ajax failed');
+         });
     };
  }
 
